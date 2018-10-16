1c0a90238d9754fe3d9433fed09ed52055a1bf35
Mikhail Varantsou <m.vorontsov@itransition.com>
2018-10-16 12:04:01 +0300

    refactor

--- retranslator.py
+++ retranslator.py
@@ -5,7 +5,7 @@ from urllib.parse import urlparse
 
 import requests
 from flask import Flask
-from flask import request
+from flask import request as flask_request
 from flask import Response
 
 
@@ -15,60 +15,59 @@ DEFAULT_MIMETYPE = 'text/html; charset=UTF-8'
 app = Flask(__name__)
 
 
-def find_urls(text):
+def replace_urls(text):
     regex = ('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F]'
              '[0-9a-fA-F]))+')
     urls = re.findall(regex, text)
-    return urls
-
-
-def get_fail_response():
-    dictionary = {'content': b'Invalid link',
-                  'headers': {},
-                  'text': 'Invalid link'}
-    return namedtuple('Response', dictionary.keys())(*dictionary.values())
-
-
-def convert_url(url):
-    parsed = urlparse(url)
-    new_url = request.url_root + url[len(parsed.scheme)+3:]
-    return new_url
-
-
-def replace_urls(text):
-    urls = find_urls(text)
     for url in urls:
-        new_url = convert_url(url)
+        parsed = urlparse(url)
+        new_url = flask_request.url_root + url[len(parsed.scheme)+3:]
         text = text.replace(url, new_url)
     return text
 
 
-def get_new_content(response):
-    mimetype = response.headers.get('Content-Type', DEFAULT_MIMETYPE)
+def get_req_link(link):
+    if '://' not in link[:10]:
+        link = 'http://' + link
+    return urlparse(link)
+
+
+def get_req_headers(link):
+    req_headers = dict(flask_request.headers)
+    host = req_headers.get('Host')
+    if host:
+        req_headers['Host'] = link.netloc
+    return req_headers
+
+
+def get_res_content(req_response):
+    mimetype = req_response.headers.get('Content-Type', DEFAULT_MIMETYPE)
     if 'text' in mimetype.lower():
-        text = replace_urls(response.text)
+        text = replace_urls(req_response.text)
         return bytes(text, 'utf-8')
 
 
-def perform_request(method, link, headers={}):
-    if not isinstance(headers, dict):
-        headers = dict(headers)
-    if '://' not in link[:10]:
-        link = 'http://' + link
-    response = requests.request(method, link, headers=headers, timeout=10)
-    return response
+def get_res_headers(req_response):
+    return dict(req_response.headers)
+
+
+def get_res_response(link):
+    method = flask_request.method
+    link = get_req_link(link)
+    req_headers = get_req_headers(link)
+    req_response = requests.request(
+        method, link.geturl(), headers=req_headers, timeout=10)
+    res_content = get_res_content(req_response)
+    res_headers = get_res_headers(req_response)
+    return Response(res_content, headers=res_headers)
 
 
 @app.route('/<path:link>', strict_slashes=False)
 def translate(link):
     try:
-        req_response = perform_request(request.method, link, request.headers)
+        res_response = get_res_response(link)
     except Exception:
-        req_response = get_fail_response()
-    
-    content = get_new_content(req_response)
-    req_headers = dict(req_response.headers)
-    res_response = Response(content, headers=req_headers)
+        res_response = Response('Invalid link', status=403)
     return res_response
 
 
